51e0afa2f95d7b20b66afbaf9e646f7eee9a003c
QuietAGuy <dev@quietaguy.com>
2020-03-18 01:53:31 +1100

    Minor change

--- argp/argp_simple.c
+++ argp/argp_simple.c
@@ -5,7 +5,7 @@
 # include <stdbool.h>
 
 /* Command syntax */
-// ./argp_example -g m -a 26 Tom
+// ./argp_example -g m/f -a 26 Tom
 
 /* Externs needed by argp */
 const char     *argp_program_version      = "argp example version 1.0" ;     // Program version
